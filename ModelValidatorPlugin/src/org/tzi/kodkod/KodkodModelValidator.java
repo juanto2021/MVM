dafc1aa5a5f398946ce41731abc9180b914faebb
utopi <utopi@JUANTO>
2022-11-23 19:19:32 +0100

    test

--- ModelValidatorPlugin/src/org/tzi/kodkod/KodkodModelValidator.java
+++ ModelValidatorPlugin/src/org/tzi/kodkod/KodkodModelValidator.java
@@ -477,7 +477,7 @@ public abstract class KodkodModelValidator {
 			// modeG = "N", se usa random para empezar por una invariante			
 			// modeG = "T" se usan todas las invariantes para unir resultados
 			String modeG="T";// Get the best results
-			modeG="G";//Pruebas
+			modeG="T";//Pruebas
 			if (debMVM) {
 				LOG.info("MVM: Start Greedy");
 			}
