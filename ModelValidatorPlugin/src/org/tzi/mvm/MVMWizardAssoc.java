3276bc695cce5d6e96367784e26cd8523acf590e
Juanto <utopick@gmail.com>
2023-11-25 20:14:42 +0100

    Convert wizard Assoc in JDialog

--- ModelValidatorPlugin/src/org/tzi/mvm/MVMWizardAssoc.java
+++ ModelValidatorPlugin/src/org/tzi/mvm/MVMWizardAssoc.java
@@ -1,21 +1,29 @@
 package org.tzi.mvm;
 import java.awt.Color;
 import java.awt.Dialog.ModalExclusionType;
+import java.awt.Dialog.ModalityType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 //import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
@@ -27,8 +35,10 @@ import javax.swing.table.JTableHeader;
 
 import org.tzi.mvm.AssocWizard;
 import org.tzi.mvm.LinkWizard;
+import org.tzi.use.gui.main.MainWindow;
+import org.tzi.use.gui.views.WizardMVMView;
 
-public class MVMWizardAssoc extends JPanel {
+public class MVMWizardAssoc extends JDialog {
 
 	/**
 	 * 
@@ -37,6 +47,8 @@ public class MVMWizardAssoc extends JPanel {
 
 	private JFrame frame;
 	private JPanel panel;
+	private JPanel pProposals;
+	private JScrollPane pScrollProposals ;
 
 	private DefaultTableModel modelTabAssocs;
 	private DefaultTableModel modelTabLinks;
@@ -62,21 +74,28 @@ public class MVMWizardAssoc extends JPanel {
 	private JButton btnApply;
 	private JButton btnExit;
 
+	private ButtonGroup group;
+
 	private ArrayList<AssocWizard> lAssocs;
 
 	private AssocWizard assocPral;
+	private String commandWizard;
 
 	private List<AssocWizard> lAssocsWizard;
 
+	private Border etchedTitledBorder;
+	private Border etchedBorder;
+
 	private int HEIGHT_TABLE = 100;
 
-	public MVMWizardAssoc(List<AssocWizard> lAssocsParent) {
-		
+	public MVMWizardAssoc(JFrame fParent, List<AssocWizard> lAssocsParent) {
+		super(fParent, "Wizard Association",ModalityType.APPLICATION_MODAL);
+
 		lAssocsWizard = lAssocsParent;
+		commandWizard="";
 		frame = new JFrame("Wizard Association");
 		frame.setAlwaysOnTop(true);
 		frame.setModalExclusionType(ModalExclusionType.TOOLKIT_EXCLUDE);
-		frame.setSize(910,490);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 
@@ -85,13 +104,12 @@ public class MVMWizardAssoc extends JPanel {
 
 		modelTabLinks = new DefaultTableModel();
 		tabLinks = new JTable(modelTabLinks);
-		//
+
 		panel = new JPanel();
 		frame.add(panel);
 
 		panel.setLayout(null);
-		//		
-		
+
 		lbAssociations = new JLabel("Associations");
 		lbAssociations.setBounds(15, 15, 100, 25);
 		panel.add(lbAssociations);	
@@ -120,7 +138,6 @@ public class MVMWizardAssoc extends JPanel {
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_UP ){
-
 					int row = tabAssocs.getSelectedRow();
 					showInfoAssocFromRow(row);
 				}
@@ -128,7 +145,6 @@ public class MVMWizardAssoc extends JPanel {
 			@Override
 			public void keyReleased(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_UP ){
-
 					int row = tabAssocs.getSelectedRow();
 					showInfoAssocFromRow(row);
 				}
@@ -183,13 +199,11 @@ public class MVMWizardAssoc extends JPanel {
 
 		Object o = tabAssocs.getValueAt(0, 0);
 		assocPral= new AssocWizard();
-		//		assocPral.setName(o.toString());
 
 		if (!o.toString().equals("")) {
 			// buscar assoc en lista			
 			assocPral=findInfoAssoc(o.toString());
 		}
-
 
 		lbCause = new JLabel("Cause");
 		lbCause.setBounds(15, 45+HEIGHT_TABLE, 100, 25);
@@ -229,33 +243,32 @@ public class MVMWizardAssoc extends JPanel {
 		showInfoLink( oLink);
 		tabLinks.setRowSelectionInterval(0, 0);
 
-		String categories[] = { "Op1", "Op2", "Op3",
-		"Op4" };
+		String borderTitle = "Proposals";
+		etchedBorder = BorderFactory.createEtchedBorder();
+		etchedTitledBorder = BorderFactory.createTitledBorder(etchedBorder, borderTitle);
 
-		JPanel pProposals = new JPanel();
-		BoxLayout layout = new BoxLayout(pProposals, BoxLayout.Y_AXIS);
-		pProposals.setLayout(layout);
-
-		for (String string : categories) {
-			pProposals.add(new JCheckBox(string));
-		}
-
-		String borderTitle = "Proposals ("+ categories.length+")";
-		Border etchedBorder = BorderFactory.createEtchedBorder();
-		Border etchedTitledBorder = BorderFactory.createTitledBorder(etchedBorder, borderTitle);
-
-		JScrollPane pScrollProposals = new JScrollPane(pProposals);
+		pScrollProposals = new JScrollPane(pProposals);
 		pScrollProposals.setBounds(15, 235+HEIGHT_TABLE, 600, 100);
 		pScrollProposals.setBorder(etchedTitledBorder);
 		panel.add(pScrollProposals);
+
+		showInfoAssocFromRow(0);
+		showInfoLinkFromRow(0);
 
 		btnApply = new JButton("Apply");
 		btnApply.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//				System.exit(0);
-				frame.dispose();
+				//	ver que option esta seleccionada
+				for (Enumeration<AbstractButton> buttons = group.getElements(); buttons.hasMoreElements();) {
+					AbstractButton button = buttons.nextElement();
+
+					if (button.isSelected()) {
+						commandWizard=button.getActionCommand();
+					}
+				}
+				dispose();
 
 			}
 		});
@@ -267,16 +280,75 @@ public class MVMWizardAssoc extends JPanel {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//				System.exit(0);
-				frame.dispose();
+				commandWizard="";
+				dispose();
 			}
 		});
 		btnExit.setBounds(760, 310+HEIGHT_TABLE, 120, 25);
 		panel.add(btnExit);
 
-		frame.setVisible(true);
+		getContentPane().add(panel);
 	}
-	//Aqui1
+
+	public String getCommandWizard() {
+		return commandWizard;
+	}
+	private String[][] createActionLinks(Map<String, String> mapActions) {
+
+		// [x][y]
+		// [x]->Acciones
+		// [y]->Comandos a devolver para su ejecucion
+		String actionLinks[][] = new String[2][2];
+		String strAction="";
+		String textAction="";
+		String strCommand="";
+		for (Map.Entry<String, String> entry : mapActions.entrySet()) {
+
+			String actionW =  entry.getKey();
+			String infoW = entry.getValue();
+
+			String pCar = actionW.substring(0,1);
+
+			switch(pCar) {
+			case "A":
+				textAction = "Assign";
+				if (strCommand!="") {
+					strCommand+="|";
+				}
+				strCommand=strCommand+"A "+infoW;
+				break;
+			case "C":
+				String sCar = actionW.substring(2,actionW.length());
+				textAction = "Create ["+sCar+"] object/s";
+				if (strCommand!="") {
+					strCommand+="|";
+				}
+				strCommand=strCommand+"C "+sCar+" "+infoW;
+				break;
+			case "I":
+				textAction = "Insert link";
+				if (strCommand!="") {
+					strCommand+="|";
+				}
+				strCommand=strCommand+"I "+infoW;
+				break;			    
+			default:
+
+			}
+
+			if (strAction!="") {
+				strAction+=", ";
+			}
+			strAction+=textAction+":"+infoW;
+		}
+
+		actionLinks[0][0] = strAction;
+		actionLinks[0][1] = strCommand;
+		//strCommand
+		actionLinks[1][0] = "Delete";
+		actionLinks[1][1] = "D";
+		return actionLinks;
+	}
 	private void showInfoAssocFromRow(int row) {
 		String nomAssoc= tabAssocs.getValueAt(row, 0).toString();
 
@@ -296,10 +368,46 @@ public class MVMWizardAssoc extends JPanel {
 		tabLinks.setRowSelectionInterval(row, row);
 
 		showInfoLink( oLink);
+		showPanelActionsLink(oLink);
 	}
 	private void showInfoLink(LinkWizard oLink) {
 		txCause.setText(oLink.getCause());
 		txFullMessage.setText(oLink.getFullMessage());
+	}
+
+	private void showPanelActionsLink(LinkWizard oLink) {
+		Map<String, String> mapActions = new HashMap<String, String>();
+		mapActions=oLink.getMapActions();
+		String actionLinks[][] = createActionLinks(mapActions);
+
+		pProposals = new JPanel();
+		BoxLayout layout = new BoxLayout(pProposals, BoxLayout.Y_AXIS);
+		pProposals.setLayout(layout);
+		group = new ButtonGroup();
+		boolean pVez = true;
+		int sizeActions=actionLinks.length;
+		for (int r=0;r<sizeActions;r++) {
+			String strAction=actionLinks[r][0];
+			String strCommand=actionLinks[r][1];
+			JRadioButton rb = new JRadioButton (strAction);
+			if (pVez) {
+				rb.setSelected(true);
+				rb.setActionCommand(strCommand);
+				pVez=false;
+			}
+			pProposals.add(rb);
+			group.add(rb);		
+		}
+
+		String borderTitle = "Proposals ("+ actionLinks.length+")";
+		panel.remove(pScrollProposals);
+		pScrollProposals = new JScrollPane(pProposals);
+		pScrollProposals.setBounds(15, 235+HEIGHT_TABLE, 600, 100);
+		etchedTitledBorder = BorderFactory.createTitledBorder(etchedBorder, borderTitle);
+		pScrollProposals.setBorder(etchedTitledBorder);
+		panel.add(pScrollProposals);
+		panel.updateUI();
+
 	}
 	private LinkWizard findInfoLink(List<LinkWizard> lLinks,int row) {
 		LinkWizard oLink = new LinkWizard();
@@ -351,50 +459,14 @@ public class MVMWizardAssoc extends JPanel {
 		tabAssocs.repaint();
 		return;
 	}
-	//	private ArrayList<AssocWizard> loadAssocs(){
-	//		lAssocs = new ArrayList<AssocWizard>();
-	//		AssocWizard ass1 = new AssocWizard("Assoc1", "Ok",null);
-	//		ass1.setlLinks(loadLinksAssoc(ass1));
-	//		AssocWizard ass2 = new AssocWizard("Assoc2", "Ok",null);
-	//		ass2.setlLinks(loadLinksAssoc(ass2));
-	//		AssocWizard ass3 = new AssocWizard("Assoc3", "Ok",null);
-	//		ass3.setlLinks(loadLinksAssoc(ass3));
-	//
-	//		lAssocs.add(ass1);
-	//		lAssocs.add(ass2);
-	//		lAssocs.add(ass3);
-	//		return lAssocs;		
-	//	}
+
 	private ArrayList<AssocWizard> loadAssocs(){
 		lAssocs = new ArrayList<AssocWizard>(lAssocsWizard);
 		return lAssocs;		
 	}
-	//	private ArrayList<LinkWizard> loadLinksAssoc(AssocWizard assoc){
-	//		ArrayList<LinkWizard> lLinks = new ArrayList<LinkWizard>();
-	//
-	//		LinkWizard link1 = new LinkWizard("matters2", "Matters","0","Teacher", assoc.getName() + " studentOf","1",
-	//				"cause1 link1 "+assoc.getName(),"fullmsg1 "+assoc.getName()+"\nLin2\nLin3\nLin4");
-	//		LinkWizard link2 = new LinkWizard("teacher3", "Teacher","0","Teacher", assoc.getName() + " studentOf","1","cause2 link2 "+assoc.getName(),"fullmsg2 "+assoc.getName());
-	//		LinkWizard link3 = new LinkWizard("teacher2", "Teacher","0","Teacher", assoc.getName() + " studentOf","1","cause3 link3 "+assoc.getName(),"fullmsg3 "+assoc.getName());
-	//
-	//		lLinks.add(link1);
-	//		lLinks.add(link2);
-	//		lLinks.add(link3);
-	//
-	//		return lLinks;
-	//	}
+
 	private ArrayList<LinkWizard> loadLinksAssoc(AssocWizard assoc){
 		ArrayList<LinkWizard> lLinks = new ArrayList<LinkWizard>(assoc.getlLinks());
-		//
-		//		LinkWizard link1 = new LinkWizard("matters2", "Matters","0","Teacher", assoc.getName() + " studentOf","1",
-		//				"cause1 link1 "+assoc.getName(),"fullmsg1 "+assoc.getName()+"\nLin2\nLin3\nLin4");
-		//		LinkWizard link2 = new LinkWizard("teacher3", "Teacher","0","Teacher", assoc.getName() + " studentOf","1","cause2 link2 "+assoc.getName(),"fullmsg2 "+assoc.getName());
-		//		LinkWizard link3 = new LinkWizard("teacher2", "Teacher","0","Teacher", assoc.getName() + " studentOf","1","cause3 link3 "+assoc.getName(),"fullmsg3 "+assoc.getName());
-		//
-		//		lLinks.add(link1);
-		//		lLinks.add(link2);
-		//		lLinks.add(link3);
-
 		return lLinks;
 	}
 	private void loadTableLinks(AssocWizard assoc) {
