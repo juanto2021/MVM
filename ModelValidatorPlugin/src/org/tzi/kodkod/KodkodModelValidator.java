0290feddda35303aeaf7e559336afbd704829d5a
juanto2021 <utopick@gmail.com>
2025-05-13 12:40:22 +0200

    test

--- ModelValidatorPlugin/src/org/tzi/kodkod/KodkodModelValidator.java
+++ ModelValidatorPlugin/src/org/tzi/kodkod/KodkodModelValidator.java
@@ -899,7 +899,7 @@ public abstract class KodkodModelValidator {
 	/**
 	 * Validates the given model.
 	 * 
-	 * @param model (Juanto-v1214)
+	 * @param model (Juanto-v1239**)
 	 */
 	public void validateVariable(IModel model, MModel mModel, Session session, String tipoSearchMSS ) {
 		this.session=session;
